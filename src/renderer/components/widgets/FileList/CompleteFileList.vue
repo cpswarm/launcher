d89bc55d0ba87478e78996615b128238ce068f8f
Junhong Liang <junhong.liang@fit.fraunhofer.de>
2018-07-31 09:42:28 +0200

    Fixed input file list also showing add new folder button

--- src/renderer/components/widgets/FileList/CompleteFileList.vue
+++ src/renderer/components/widgets/FileList/CompleteFileList.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="overlay" v-show="!enabled"></div>
     <file-list class="file-list" :path="path" :enabled="enabled" @state-changed="stateChanged" :watchFile="listProperties.watchFile" :watchDir="listProperties.watchDir" :multiSelect="listProperties.multiSelect" @folder-selected="folderSelected" @folder-deleted="folderDeleted" @error="emitError"></file-list>
-    <add-file-box class="add-file-box" :path="path" @error="emitError"></add-file-box>
+    <add-file-box class="add-file-box" v-if="listProperties.allowAdd" :path="path" @error="emitError"></add-file-box>
   </div>
 </template>
 
